c2cdda9ef1040e4038e6eb4c36658aa9d4393a68
Jesutofunmi <67161205+tofydebo@users.noreply.github.com>
2021-02-02 16:21:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const menuToggle = document.querySelector('.burger')
-const dropdown = document. querySelector('.list1')
+const dropdown = document.querySelector('.list1')
                                          
    menuToggle.addEventListener('click', () =>{
     menuToggle.classList.toggle('active')
